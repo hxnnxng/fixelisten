926037a3bd868f4b1161891529cdbf67dd912cef
Henning Schmidt <hs170674@gmail.com>
2026-05-17 08:23:10 +0200

    Add PWA icons and manifest for home screen support

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,12 @@
 import './globals.css';
 
-export const metadata = { title: 'Fixelisten', description: 'Ting der skal ordnes' };
+export const metadata = {
+  title: 'Fixelisten',
+  description: 'Ting der skal ordnes',
+  manifest: '/manifest.json',
+  icons: { apple: '/icon-192.png', icon: '/icon-512.png' },
+  appleWebApp: { capable: true, statusBarStyle: 'default', title: 'Fixelisten' },
+};
 
 export default function RootLayout({ children }) {
   return <html lang="da"><body>{children}</body></html>;
